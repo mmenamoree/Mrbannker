d370e66fbb45dd6b34bf43f80c7462e28c3caa4b
mmenamore00 <111655808+mmenamore00@users.noreply.github.com>
2022-08-21 12:03:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,8 +39,8 @@ BOT_ID = bot_info.id
 
 # USE YOUR ROTATING PROXY API IN DICT FORMAT http://user:pass@providerhost:port
 proxies = {
-           'http': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/',
-           'https': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/'
+           'http': 'http://irkltmyj-rotate:f2oyleso9r2u@p.webshare.io:80/',
+           'https': 'http://irkltmyj-rotate;f2oyleso9r2u@p.webshare.io:80/'
 }
 
 session = requests.Session()
